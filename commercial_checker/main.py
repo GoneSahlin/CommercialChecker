5501cc5d42bf6e7d4b7cbd6005e58f9e81a63221
GoneSahlin <zach@sahlins.net>
2024-04-30 23:40:13 -0700

    testing ipytv

--- commercial_checker/main.py
+++ commercial_checker/main.py
@@ -1,14 +1,17 @@
-from m3u_parser import M3uParser
+from ipytv import playlist
+
 
 url = "https://iptv-org.github.io/iptv/index.m3u"
-useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+pl = playlist.loadu(url)
+print(pl.length())
 
-# Instantiate the parser
-parser = M3uParser(timeout=5, useragent=useragent)
 
-# Parse the m3u file
-parser.parse_m3u(url)
+for channel in pl:
+  if "tnt" == channel.name.lower():
+    tnt = channel
 
-# Get the list of streams
-print(len(parser.get_list()))
+print(tnt.attributes)
+print(tnt.name)
+
+print(tnt.url)
 
